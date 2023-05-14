09ce8c46ec3d1cca391fc3bfb8e2be4a39f70499
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-05-14 10:17:40 +0530

    api rendering

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,12 +1,32 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import BTC from "./assets/btc.svg";
 import AVAX from "./assets/avax.svg";
 import { useEffect } from "react";
+
 type Props = {};
 
 const Table = (props: Props) => {
+  const [asset1, setAsset1] = useState("");
+  const [amount1, setAmount1] = useState("");
+  const [refearn1, setRefearn1] = useState("");
+  const [user1, setUser1] = useState("");
+  const [asset2, setAsset2] = useState("");
+  const [amount2, setAmount2] = useState("");
+  const [refearn2, setRefearn2] = useState("");
+  const [user2, setUser2] = useState("");
+  const [status1, setStatus1] = useState("");
+  const [status2, setStatus2] = useState("");
+  const [type1, setType1] = useState("");
+  const [type2, setType2] = useState("");
+
+  function formatAddress(address: string): string {
+    const prefix = address.slice(0, 4);
+    const suffix = address.slice(-8);
+    return `${prefix}...${suffix}`;
+  }
+
   async function getData() {
     const res = await fetch(
       "https://raw.githubusercontent.com/akshita151199/APIs/main/data"
@@ -15,7 +35,22 @@ const Table = (props: Props) => {
       throw new Error("Failed to fetch data");
     }
     const data = await res.json();
-    console.log(data);
+    const result = data.data;
+
+    setAsset1(result[0].asset);
+    setAmount1(result[0].amount);
+    setRefearn1(result[0].referral_earnings);
+    setUser1(formatAddress(result[0].user));
+
+    setAsset2(result[1].asset);
+    setAmount2(result[1].amount);
+    setRefearn2(result[1].referral_earnings);
+    setUser2(formatAddress(result[1].user));
+    setStatus1(result[0].state);
+    setStatus2(result[1].state);
+    setType1(result[0].type);
+    setType2(result[1].type);
+
     return data;
   }
   useEffect(() => {
@@ -58,21 +93,21 @@ const Table = (props: Props) => {
                 >
                   <Image src={BTC} height={20} width={30} alt="" />
                   <div className="flex-col">
-                    <p className="text-left">Bitcoin</p>
+                    <p className="text-left">{asset1}</p>
                     <div className="flex gap-2">
-                      <p className="text-xs text-gray-400">Put</p>
+                      <p className="text-xs text-gray-400">{type1}</p>
                       <Image src={AVAX} alt="" />
                     </div>
                   </div>
                 </th>
 
                 <td className="px-6 py-4 flex-col">
-                  <p>0.0000 BNB</p>
-                  <p className="text-xs text-gray-400">Expired</p>
+                  <p>{amount1} BNB</p>
+                  <p className="text-xs text-gray-400">{status1}</p>
                 </td>
-                <td className="px-6 py-4">0xFbE..0f58A7D</td>
+                <td className="px-6 py-4">{user1}</td>
                 <td className="px-6 py-4 flex-col">
-                  <p>0.000.BNB</p>
+                  <p>{refearn1} BNB</p>
                   <p className="text-xs text-gray-400 cursor-pointer">
                     View on BSC Scan
                   </p>
@@ -85,21 +120,21 @@ const Table = (props: Props) => {
                 >
                   <Image src={BTC} height={20} width={30} alt="" />
                   <div className="flex-col">
-                    <p className="text-left">Bitcoin</p>
+                    <p className="text-left">{asset2}</p>
                     <div className="flex gap-2">
-                      <p className="text-xs text-gray-400">Put</p>
+                      <p className="text-xs text-gray-400">{type2}</p>
                       <Image src={AVAX} alt="" />
                     </div>
                   </div>
                 </th>
 
                 <td className="px-6 py-4 flex-col">
-                  <p>0.0000 BNB</p>
-                  <p className="text-xs text-gray-400">Expired</p>
+                  <p>{amount2} BNB</p>
+                  <p className="text-xs text-gray-400">{status2}</p>
                 </td>
-                <td className="px-6 py-4">0xFbE..0f58A7D</td>
+                <td className="px-6 py-4">{user2}</td>
                 <td className="px-6 py-4 flex-col">
-                  <p>0.000.BNB</p>
+                  <p>{refearn2} BNB</p>
                   <p className="text-xs text-gray-400 cursor-pointer">
                     View on BSC Scan
                   </p>
